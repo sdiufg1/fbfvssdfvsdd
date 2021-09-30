6f65f0861af6caf13597cfce5ffe7857694e5545
sdiufg1 <91625578+sdiufg1@users.noreply.github.com>
2021-09-30 19:31:51 +0200

    None wtf why thas not working

--- main.py
+++ main.py
@@ -100,18 +100,16 @@ async def snipe(message):
 
 # Fin du code de la commande snipe.
 
-variable = ""
-
 # Debut de la commande pour le help.
 @client.command()
-async def help(ctx, message = variable):
+async def help(ctx, message = None):
     global none
     if message == "snipe":
         embed=discord.Embed(title="Snipe Command", description="Cette commande sert a reccuperer le dernier message supprimer par un uttilisateur", color=0x07caf3)
         embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/871526418578825226.png?v=1")
         embed.add_field(name="uttilisation:", value="`+snipe`", inline=True)
         await ctx.send(embed=embed)
-    if message == variable:
+    if message == None:
         global heures
         embed=discord.Embed(title="Liste des commandes 🍏", description="**Prefix** `+`\n**Plus d'information** `+help <command>`", color=0x3ee378)
         embed.set_thumbnail(url="https://c.tenor.com/_DOAnKatq7EAAAAM/cod-chost.gif")
