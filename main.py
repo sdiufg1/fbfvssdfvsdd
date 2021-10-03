51f96dc0acbb3d170cc4dee349ce465748fae319
Your Name <yorickpayen06@gmail.com>
2021-10-03 05:53:37 +0200

    vesz

--- main.py
+++ main.py
@@ -191,13 +191,12 @@ async def cmd(ctx, message = None):
 # Debut de la commande help
 @client.command()
 async def help(ctx):
-        global heures
         embed=discord.Embed(title="Liste des commandes 🍏", description="**Prefix** `+`\n**Plus d'information** `+help <command>`", color=0x3ee378)
         embed.set_thumbnail(url="https://c.tenor.com/_DOAnKatq7EAAAAM/cod-chost.gif")
         embed.add_field(name="🤷‍♂️・User", value="`help` `ping` `pp` `say`", inline=False)
         embed.add_field(name="👌・Mod", value="`snipe` `nuke` `mute` `voicemove` `lock` `unlock`", inline=False)
         embed.add_field(name="🎉・Fun", value="`emote` `chinese` `coinflip` `cat` `hug` `dog` `duck`", inline=False)
-        embed.set_footer(text=f"Commande envoyer par {ctx.author.name} à {heures}", icon_url = ctx.author.avatar_url)
+        embed.set_footer(text=f"Commande envoyer par {ctx.author.name} à ", icon_url = ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
 
