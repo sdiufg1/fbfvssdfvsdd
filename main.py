42a358156bcc56c85c7f3772899d210cc25f06b3
sdiufg1 <91625578+sdiufg1@users.noreply.github.com>
2021-10-22 19:46:39 +0200

    M

--- main.py
+++ main.py
@@ -102,13 +102,13 @@ async def snipe(message):
 
 # Debut de la commande pour le help.
 @client.command()
-async def help(ctx, message = "L"):
+async def help(ctx, message = None):
     if message == "snipe":
         embed=discord.Embed(title="Snipe Command", description="Cette commande sert a reccuperer le dernier message supprimer par un uttilisateur", color=0x07caf3)
         embed.set_thumbnail(url="https://cdn.discordapp.com/emojis/871526418578825226.png?v=1")
         embed.add_field(name="uttilisation:", value="`+snipe`", inline=True)
         await ctx.send(embed=embed)
-    if message == "L":
+    if message == None:
         embedinfo = discord.Embed()
         global heures
         USer = ctx.author
